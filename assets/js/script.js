18432b58b68cf5ae2c736f16a167cedbd5342975
WeiMingTay <110397919+WeiMingTay@users.noreply.github.com>
2022-08-20 11:41:31 +0200

    namen aendern, bg-img

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+// Namen ändern
+
+function changeHomeName() {
+    let HomeName = prompt("Change Home Name to", "Hamburg Towers")
+    if (HomeName != null) {
+        document.getElementById("nameHome").innerText=HomeName;
+    }
+}
+function changeAwayName() {
+    let AwayName = prompt("Change Away Name to", "Chicago")
+    if (AwayName != null) {
+        document.getElementById("nameAway").innerText=AwayName;
+    }
+}
+
+// =========================================
 
 // Freiwürfe
 function pointHome1() {
@@ -17,32 +33,32 @@ function pointAway1() {
 
 // 2 Punkt Wurf
 function pointHome2() {
-  var einenPunkt = Number(document.getElementById("scoreHome").innerHTML);
-  einenPunkt += 2;
+  var zweiPunkte = Number(document.getElementById("scoreHome").innerHTML);
+  zweiPunkte += 2;
 
-  document.getElementById("scoreHome").innerHTML = einenPunkt;
+  document.getElementById("scoreHome").innerHTML = zweiPunkte;
 }
 function pointAway2() {
-  var einenPunkt = Number(document.getElementById("scoreAway").innerHTML);
-  einenPunkt += 2;
+  var zweiPunkte = Number(document.getElementById("scoreAway").innerHTML);
+  zweiPunkte += 2;
 
-  document.getElementById("scoreAway").innerHTML = einenPunkt;
+  document.getElementById("scoreAway").innerHTML = zweiPunkte;
 }
 
 // =================
 
 // 3 Punkt Wurf
 function pointHome3() {
-  var einenPunkt = Number(document.getElementById("scoreHome").innerHTML);
-  einenPunkt += 3;
+  var dreiPunkte = Number(document.getElementById("scoreHome").innerHTML);
+  dreiPunkte += 3;
 
-  document.getElementById("scoreHome").innerHTML = einenPunkt;
+  document.getElementById("scoreHome").innerHTML = dreiPunkte;
 }
 function pointAway3() {
-  var einenPunkt = Number(document.getElementById("scoreAway").innerHTML);
-  einenPunkt += 3;
+  var dreiPunkte = Number(document.getElementById("scoreAway").innerHTML);
+  dreiPunkte += 3;
 
-  document.getElementById("scoreAway").innerHTML = einenPunkt;
+  document.getElementById("scoreAway").innerHTML = dreiPunkte;
 }
 
 // =========================================
